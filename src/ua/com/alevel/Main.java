7c473e6c8c78f3b58e5173f28455a0721a6339a0
funtushan@gmail.com <funtushan@gmail.com>
2021-03-03 21:06:58 +0200

    jjj

--- src/ua/com/alevel/Main.java
+++ src/ua/com/alevel/Main.java
@@ -4,6 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("true = " + true);
-        System.out.println("true = " + true);
     }
 }
--- src/ua/com/alevel/Test.java
+++ src/ua/com/alevel/Test.java
@@ -1,2 +1,4 @@
-package ua.com.alevel;public class Test {
+package ua.com.alevel;
+
+public class Test {
 }
